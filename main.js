51ee04002934d3106198166abd0b9f70cccf3c9d
Aravind Natchiappan <aravind.natch@gmail.com>
2024-11-26 22:58:22 +0000

    add tracks history

--- main.js
+++ main.js
@@ -3,14 +3,32 @@ const axios = require('axios');
 const apicache = require('apicache');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { Sequelize, DataTypes } = require('sequelize');
+const { CronJob } = require('cron');
 
 dotenv.config();
 
 const app = express();
-app.use(cors( { origin: ['https://aravindnatch.me'] } ));
+app.use(cors({ origin: ['https://aravindnatch.me', 'http://localhost:3000'] }));
 const cache = apicache.middleware;
 
-const refreshToken = process.env.refresh_token
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './tracks.db',
+  logging: false,
+});
+
+const Play = sequelize.define('Play', {
+  played_at: {
+    type: DataTypes.DATE,
+    unique: true,
+  },
+  data: DataTypes.JSON,
+});
+
+sequelize.sync();
+
+const refreshToken = process.env.refresh_token;
 const clientId = process.env.client_id;
 const clientSecret = process.env.client_secret;
 
@@ -32,34 +50,88 @@ const getToken = async () => {
   return data.access_token;
 };
 
-const getCurrentTrack = async (token) => {
-  const options = {
-    url: 'https://api.spotify.com/v1/me/player/currently-playing',
-    headers: { Authorization: `Bearer ${token}` },
-  };
-
+const getRecentlyPlayedTracks = async (token) => {
   try {
-    const { data } = await axios(options);
-    return data.item;
+    const { data } = await axios.get("https://api.spotify.com/v1/me/player/recently-played?limit=50", {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    if (data && data.items) {
+      for (const item of data.items) {
+        const playedAt = new Date(item.played_at);
+
+        await Play.findOrCreate({
+          where: { played_at: playedAt },
+          defaults: { data: item },
+        });
+      }
+    }
   } catch (err) {
-    console.error(err);
-    return null;
+    console.error(err)
   }
 };
 
-const cacheMiddleware = cache('1 minute');
-
-app.get('/spotify/current', cacheMiddleware, async (_req, res) => {
+const job = new CronJob('0 * * * *', async () => {
   const token = await getToken();
-  const track = await getCurrentTrack(token);
+  await getRecentlyPlayedTracks(token);
+});
 
-  if (track) {
-    res.json(track);
-  } else {
+job.start();
+
+app.get('/', (_req, res) => {
+  res.redirect('https://aravindnatch.me');
+});
+
+app.get('/spotify/current', cache('5 seconds'), async (_req, res) => {
+  const token = await getToken();
+
+  try {
+    const { data } = await axios.get("https://api.spotify.com/v1/me/player/currently-playing", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    if (data && data.item) {
+      res.json(data);
+      return;
+    }
     res.send(null);
+  } catch (err) {
+    console.error(err.message);
+
+    const errorResponse = err.response ? {
+      status: err.response.status,
+      statusText: err.response.statusText,
+      data: err.response.data,
+    } : 'Internal server error';
+
+    res.status(500).send(errorResponse);
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.get('/tracks', async (_req, res) => {
+  try {
+    const tracks = await Play.findAll({
+      order: [['played_at', 'DESC']],
+    });
+
+    const response = [];
+    tracks.forEach((track) => {
+      const { name, artists, album } = track.data.track;
+      response.push({
+        name,
+        artists: artists.map((artist) => artist.name).join(', '),
+        album: album.name,
+        album_cover: album.images[0].url,
+        played_at: track.played_at,
+      });
+    });
+
+    res.json(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+app.listen(33329, () => {
+  console.log('Server listening on port 33329');
 });
